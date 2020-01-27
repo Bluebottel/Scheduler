eece3efa50e342e586d13f25fef19fe927d991b3
Saskian <bluebottel@gmail.com>
2020-01-27 14:55:54 +0100

    Typo and added solo export => default

--- src/colors.js
+++ src/colors.js
@@ -7,6 +7,6 @@ colorMap.set(4, '#cc9043') // brown
 colorMap.set(5, '#f00')    // bright red
 colorMap.set(6, '#3697ff') // bright blue
 colorMap.set(7, '#7b7b7b') // grey
-colorMap.set(7, '#49b717') // poison green
+colorMap.set(8, '#49b717') // poison green
 
-export { colorMap }
+export default colorMap
