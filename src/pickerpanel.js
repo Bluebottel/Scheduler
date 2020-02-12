341bb8280baa4cab5bc55d24f46d430d06dc6fc3
Saskian <bluebottel@gmail.com>
2020-02-12 14:01:58 +0100

    Handling empty resource/shift lists gracefully, first step

--- src/pickerpanel.js
+++ src/pickerpanel.js
@@ -11,63 +11,77 @@ class PickerPanel extends Component {
     super(props)
   }
 
+  renderShifts = () => {
+    return (
+      this.props.shifts.map((shift, i) => {
+	return (
+	  <div
+	    className =
+	    {
+	      `${shift.id === this.props.selected.shift.id ? ' selected' : ''}`
+	      + ' option clickable'
+	    }
+	    onClick = {() => {
+		
+		this.setState({
+		  selected: update(this.props.selected, { shift: {$set: shift}})
+		})
+		
+		this.props.setSelected({
+		  shift: shift,
+		  resource: this.props.selected.resource
+		})
+	    }}
+	    key = { i } >
+	    <div className = "shiftTitle">{ shift.title }</div>
+	    <ShiftInfoBlock className = "shiftInfo" shift = { shift } />
+	  </div>
+	)
+	
+      })
+    )
+  }
+
+  renderResources = () => {
+    return (
+      this.props.resources.map((res, i) => {
+	return (
+	  <div
+	    className =
+	    {
+	      `${res.id === this.props.selected.resource.id ? ' selected' : ''}`
+	      + ' option clickable'
+	    }
+	    
+	    onClick = {() => {
+		this.setState({
+		  selected: update(this.props.selected, { resource: {$set: res}})
+		})
+
+		this.props.setSelected({
+		  shift: this.props.selected.shift,
+		  resource: res,
+		})
+	    }}
+	    key = { i } >
+	    { res.title }
+	  </div>
+	)
+      }) 
+    )
+  }
+
   render() {
     return (
       <div id = "sideContainer">
 	<div className = "boxLabel">Pass</div>
 	<div className = "pickerBox">
-	  {
-	    this.props.shifts.map((shift, i) => {
-	      return (
-		<div
-		  className =
-		  {`option${shift.id === this.props.selected.shift.id ? ' selected' : ''}`}
-		  onClick = {() => {
-		      
-		      this.setState({
-			selected: update(this.props.selected, { shift: {$set: shift}})
-		      })
-		      
-		      this.props.setSelected({
-			shift: shift,
-			resource: this.props.selected.resource
-		      })
-		  }}
-		  key = { i } >
-		  <div className = "shiftTitle">{ shift.title }</div>
-		  <ShiftInfoBlock className = "shiftInfo" shift = { shift } />
-		</div>
-	      )
-	      
-	  })
-	  }
+	  { this.renderShifts() }
       </div>
       
       <div className = "boxLabel">Personer</div>
       <div className = "pickerBox">
-      {
-	this.props.resources.map((res, i) => {
-	  return (
-	    <div
-	    className =
-	      {`option${res.id === this.props.selected.resource.id ? ' selected' : ''}`}
-	    
-	    onClick = {() => {
-	      this.setState({
-		selected: update(this.props.selected, { resource: {$set: res}})
-	      })
-
-	      this.props.setSelected({
-		shift: this.props.selected.shift,
-		resource: res,
-	      })
-	    }}
-	    key = { i } >
-	    { res.title }
-      </div>
-	  )
-	}) 
-      }
+	{ this.renderResources() }
       </div>
       <button onClick = { () => this.props.setOptionsModal(true) }>
 	Options
@@ -94,6 +108,7 @@ function ShiftInfoBlock(props) {
     </div>
   )
 }
+
 
 
 export default PickerPanel
